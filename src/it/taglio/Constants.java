ab0abdabfd8d742702db61dec6efecf2275b1fd4
Sandro Massa <37407738+EnlitHamster@users.noreply.github.com>
2019-02-21 09:19:59 +0100

    Added versioning & cleanup

--- src/it/taglio/Constants.java
+++ src/it/taglio/Constants.java
@@ -6,14 +6,15 @@ public class Constants {
 
 	public static final char sep = File.separatorChar;
 	public static final File root = new File(System.getProperty("user.home") + sep + "udngui" + sep);
-	public static final String dep_dir = "/res/dependencies/";
-	public static final String ass_dir = "/res/assets/";
 	public static final String[] deps = { "dumpbin.exe", "link.exe", "undname.exe", "mspdbcore.dll" };
 	
+	public static final String dep_dir = "/res/dependencies/";
+	public static final String ass_dir = "/res/assets/";
 	public static final String icon = ass_dir + "icon.png";
 	public static final String top = ass_dir + "top.png";
 	public static final String info = ass_dir + "info.png";
 	public static final String unknown = ass_dir + "unknown.png";
 	public static final String entry = ass_dir + "entry.png";
+	public static final String v = ".v";
 	
 }
--- src/it/taglio/Main.java
+++ src/it/taglio/Main.java
@@ -4,17 +4,27 @@ import static it.taglio.Constants.dep_dir;
 import static it.taglio.Constants.deps;
 import static it.taglio.Constants.root;
 import static it.taglio.Constants.sep;
+import static it.taglio.Constants.v;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
 
 import javax.swing.JOptionPane;
 
 import it.taglio.gui.UDNGui;
 
 public class Main {
+
+	private static final String version = "0.3-alpha-01u";
 
 	public static void main(String[] args) {
 		if (!System.getProperty("os.name").contains("Windows")) {
@@ -24,17 +34,21 @@ public class Main {
 			System.exit(1);
 		}
 
-		if (!root.exists())
-			try {
-				root.mkdirs();
-			} catch (SecurityException se) {
-				JOptionPane.showMessageDialog(null,
-						"Couldn't create necessary directories. Try to launch as administrator.", "Error",
-						JOptionPane.ERROR_MESSAGE);
-				System.err.println("Couldn't create necessary directories. Try to launch as administrator.");
-				se.printStackTrace();
-				System.exit(1);
-			}
+		if (!root.exists() || !valid())
+			if (root.exists())
+				clean(root.toPath());
+
+		try {
+			root.mkdirs();
+			validate();
+		} catch (Exception se) {
+			JOptionPane.showMessageDialog(null,
+					"Couldn't create necessary directories. Try to launch as administrator.", "Error",
+					JOptionPane.ERROR_MESSAGE);
+			System.err.println("Couldn't create necessary directories. Try to launch as administrator.");
+			clean(root.toPath());
+			System.exit(1);
+		}
 
 		for (String dep : deps) {
 			if (!((new File(root.getPath() + sep + dep)).exists())) {
@@ -51,6 +65,29 @@ public class Main {
 		}
 
 		new UDNGui();
+	}
+
+	private static boolean valid() {
+		File versioning = new File(root.getPath() + sep + v);
+		if (!versioning.exists())
+			return false;
+
+		BufferedReader reader = null;
+
+		try {
+			reader = new BufferedReader(new FileReader(versioning));
+			return reader.readLine().equals(version);
+		} catch (Exception e) {
+			return false;
+		} finally {
+			try {
+				reader.close();
+			} catch (Exception e) {
+				JOptionPane.showMessageDialog(null, "Unable to properly read version!", "Error",
+						JOptionPane.ERROR_MESSAGE);
+				System.exit(1);
+			}
+		}
 	}
 
 	private static void extract(String path, String file) throws Exception {
@@ -74,6 +111,48 @@ public class Main {
 		} finally {
 			stream.close();
 			output.close();
+		}
+	}
+
+	private static void clean(Path path) {
+		try {
+			if (Files.isDirectory(path, LinkOption.NOFOLLOW_LINKS)) {
+				DirectoryStream<Path> entries = Files.newDirectoryStream(path);
+				for (Path entry : entries) {
+					clean(entry);
+				}
+			}
+
+			Files.delete(path);
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Unable to clear program directory", "Error",
+					JOptionPane.ERROR_MESSAGE);
+			System.exit(1);
+		}
+	}
+	
+	private static void validate() {
+		FileWriter writer = null;
+		
+		try {
+			File file = new File(root.getPath() + sep + v);
+			writer = new FileWriter(file);
+			writer.write(version);
+			writer.close();
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Unable to validate installation", "Error",
+					JOptionPane.ERROR_MESSAGE);
+			clean(root.toPath());
+			System.exit(1);
+		} finally {
+			try {
+				writer.close();
+			} catch (Exception e) {
+				JOptionPane.showMessageDialog(null, "Unable to validate installation", "Error",
+						JOptionPane.ERROR_MESSAGE);
+				clean(root.toPath());
+				System.exit(1);
+			}
 		}
 	}
 
